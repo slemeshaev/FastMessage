c7be88a7c63d6de43f48e40a68a07ad9115d8e63
slemeshaev <lemeshaevs@gmail.com>
2020-12-21 17:33:52 +0300

    Configure project and setup home screen

--- FastMessage/FastMessage/Controller/ConversationsController.swift
+++ FastMessage/FastMessage/Controller/ConversationsController.swift
@@ -5,4 +5,35 @@
 //  Created by Станислав Лемешаев on 21.12.2020.
 //
 
-import Foundation
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    @objc func showProfile() {
+        print(123)
+    }
+    
+    // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .white
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Сообщения"
+        
+        let image = UIImage(systemName: "person.circle.fill")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(showProfile))
+    }
+}
+
