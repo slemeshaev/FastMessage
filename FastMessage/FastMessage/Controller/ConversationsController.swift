6fd00f21d28b3eff829e46844eb30e7fdec4b1a4
slemeshaev <lemeshaevs@gmail.com>
2020-12-21 17:47:11 +0300

    Setup Home Screen Navigation Bar

--- FastMessage/FastMessage/Controller/ConversationsController.swift
+++ FastMessage/FastMessage/Controller/ConversationsController.swift
@@ -26,14 +26,31 @@ class ConversationsController: UIViewController {
     func configureUI() {
         view.backgroundColor = .white
         
-        navigationController?.navigationBar.prefersLargeTitles = true
-        navigationItem.title = "Сообщения"
+        configureNavigationBar()
         
         let image = UIImage(systemName: "person.circle.fill")
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: image,
                                                            style: .plain,
                                                            target: self,
                                                            action: #selector(showProfile))
+    }
+    
+    func configureNavigationBar() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Сообщения"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
     }
 }
 
