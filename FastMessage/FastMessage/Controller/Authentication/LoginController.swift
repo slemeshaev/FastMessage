1bdcbda4d6a1fe5e122a91a46027ac71822c28d0
slemeshaev <lemeshaevs@gmail.com>
2020-12-22 18:30:22 +0300

    Setup Login Screen & Intro to AutoLayout

--- FastMessage/FastMessage/Controller/Authentication/LoginController.swift
+++ FastMessage/FastMessage/Controller/Authentication/LoginController.swift
@@ -5,4 +5,46 @@
 //  Created by Станислав Лемешаев on 22.12.2020.
 //
 
-import Foundation
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    private let iconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "bubble.right")
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        configureGradientLayer()
+        view.addSubview(iconImage)
+        iconImage.translatesAutoresizingMaskIntoConstraints = false
+        iconImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        iconImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        iconImage.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        iconImage.widthAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
+    func configureGradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor,
+                           UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
