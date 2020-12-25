1a049fa81104150d1bebfa6ab26fff5a44955ec8
slemeshaev <lemeshaevs@gmail.com>
2020-12-25 23:36:43 +0300

    Did form validation

--- FastMessage/FastMessage/ViewModel/LoginViewModel.swift
+++ FastMessage/FastMessage/ViewModel/LoginViewModel.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct LoginViewModel {
+    var email: String?
+    var password: String?
+    
+    var formIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false
+    }
+}
