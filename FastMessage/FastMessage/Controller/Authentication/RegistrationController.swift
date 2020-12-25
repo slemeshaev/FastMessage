8c41661658457cdcd0cb04ea6441cee178b6f5fc
slemeshaev <lemeshaevs@gmail.com>
2020-12-26 00:08:24 +0300

    Authentication Protocol

--- FastMessage/FastMessage/Controller/Authentication/RegistrationController.swift
+++ FastMessage/FastMessage/Controller/Authentication/RegistrationController.swift
@@ -17,6 +17,7 @@ class RegistrationController: UIViewController {
         button.setImage(#imageLiteral(resourceName: "plus_button"), for: .normal)
         button.tintColor = .white
         button.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
+        button.clipsToBounds = true
         return button
     }()
     
@@ -92,7 +93,9 @@ class RegistrationController: UIViewController {
     
     // MARK: - Selectors
     @objc func handleSelectPhoto() {
-        print(#function)
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
     }
     
     @objc func handleSignUpButton() {
@@ -129,5 +132,19 @@ class RegistrationController: UIViewController {
         view.addSubview(alreadyHaveAccountButton)
         alreadyHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor,
                                         paddingLeft: 32, paddingBottom: 16, paddingRight: 32)
+    }
+}
+
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension RegistrationController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        plusPhotoButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        
+        plusPhotoButton.layer.borderColor = UIColor.white.cgColor
+        plusPhotoButton.layer.borderWidth = 3.0
+        plusPhotoButton.layer.cornerRadius = 200 / 2
+        
+        dismiss(animated: true, completion: nil)
     }
 }
--- FastMessage/FastMessage/ViewModel/Authentication/RegistrationViewModel.swift
+++ FastMessage/FastMessage/ViewModel/Authentication/RegistrationViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+protocol AuthenticationProtocol {
+    var formIsValid: Bool { get }
+}
+
+struct RegistrationViewModel: AuthenticationProtocol {
+    var email: String?
+    var password: String?
+    
+    var formIsValid: Bool {
+        return email?.isEmpty == false && password?.isEmpty == false
+    }
+}
