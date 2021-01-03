4e0e8e173c8f63df44d8c22b53194f1e3729bc49
slemeshaev <lemeshaevs@gmail.com>
2021-01-03 11:21:08 +0300

    User data model and Fetching User Info from Firestore

--- FastMessage/FastMessage/API/Service.swift
+++ FastMessage/FastMessage/API/Service.swift
@@ -5,4 +5,17 @@
 //  Created by Станислав Лемешаев on 03.01.2021.
 //
 
-import Foundation
+import Firebase
+
+struct Service {
+    
+    static func fetchUsers() {
+        Firestore.firestore().collection("users").getDocuments { snapshot, error in
+            snapshot?.documents.forEach({ document in
+                print(document.data())
+            })
+        }
+    }
+    
+    
+}
--- FastMessage/FastMessage/Model/User.swift
+++ FastMessage/FastMessage/Model/User.swift
@@ -5,4 +5,10 @@
 //  Created by Станислав Лемешаев on 03.01.2021.
 //
 
-import Foundation
+struct User {
+    let uid: String
+    let profileImageUrl: String
+    let userName: String
+    let fullName: String
+    let email: String
+}
