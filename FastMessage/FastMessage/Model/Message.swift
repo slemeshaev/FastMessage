9f4803240ae35aa3ecef00e57acf0c480fa4e61c
slemeshaev <lemeshaevs@gmail.com>
2021-01-09 02:25:20 +0300

    Creating files Message and MessageViewModel

---
+++ FastMessage/FastMessage/Model/Message.swift
@@ -0,0 +1,8 @@
+//
+//  Message.swift
+//  FastMessage
+//
+//  Created by Станислав Лемешаев on 09.01.2021.
+//
+
+import Foundation
---
+++ FastMessage/FastMessage/ViewModel/MessageViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MessageViewModel.swift
+//  FastMessage
+//
+//  Created by Станислав Лемешаев on 09.01.2021.
+//
+
+import Foundation
