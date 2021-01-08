c2d1b396117525adaf8f7b0e4acbafe6a6fcd762
slemeshaev <lemeshaevs@gmail.com>
2021-01-09 02:25:34 +0300

    Sending test messages

--- FastMessage/FastMessage/Model/Message.swift
+++ FastMessage/FastMessage/Model/Message.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+struct Message {
+    let text: String
+    let isFromCurrentUser: Bool
+}
--- FastMessage/FastMessage/ViewModel/MessageViewModel.swift
+++ FastMessage/FastMessage/ViewModel/MessageViewModel.swift
@@ -5,4 +5,22 @@
 //  Created by Станислав Лемешаев on 09.01.2021.
 //
 
-import Foundation
+import UIKit
+
+struct MessageViewModel {
+    
+    private let message: Message
+    
+    var messageBackgroundColor: UIColor {
+        return message.isFromCurrentUser ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) : .systemBlue
+    }
+    
+    var messageTextColor: UIColor {
+        return message.isFromCurrentUser ? .black : .white
+    }
+    
+    init(message: Message) {
+        self.message = message
+    }
+    
+}
