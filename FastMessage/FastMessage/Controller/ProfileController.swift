d466c4ea60dc5f734e931f949698489e211aabb6
slemeshaev <lemeshaevs@gmail.com>
2021-01-15 20:45:05 +0300

    Created ProfileHeader

---
+++ FastMessage/FastMessage/Controller/ProfileController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileController.swift
+//  FastMessage
+//
+//  Created by Станислав Лемешаев on 15.01.2021.
+//
+
+import Foundation
---
+++ FastMessage/FastMessage/View/Header/ProfileHeader.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileHeader.swift
+//  FastMessage
+//
+//  Created by Станислав Лемешаев on 15.01.2021.
+//
+
+import Foundation
