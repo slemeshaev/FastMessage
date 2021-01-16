f442b7c6d41697408ea9f353704ed6967b9b4118
slemeshaev <lemeshaevs@gmail.com>
2021-01-16 11:27:59 +0300

    Work with ProfileController and ProfileHeader

--- FastMessage/FastMessage/Controller/ProfileController.swift
+++ FastMessage/FastMessage/Controller/ProfileController.swift
@@ -11,13 +11,22 @@ class ProfileController: UITableViewController {
     
     // MARK: - Properties
     
-    private lazy var headerView = ProfileHeader(frame: .init(x: 0, y: 0, width: view.frame.width, height: 380))
+    private static let reuseId = "ProfileCell"
+    private lazy var headerView = ProfileHeader(frame: .init(x: 0, y: 0,
+                                                             width: view.frame.width,
+                                                             height: 380))
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
     }
     
     // MARK: - Selectors
@@ -28,6 +37,24 @@ class ProfileController: UITableViewController {
     
     func configureUI() {
         tableView.backgroundColor = .white
+        
+        tableView.tableHeaderView = headerView
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: ProfileController.reuseId)
+        tableView.tableFooterView = UIView()
+        tableView.contentInsetAdjustmentBehavior = .never
     }
     
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ProfileController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileController.reuseId, for: indexPath)
+        return cell
+    }
 }
