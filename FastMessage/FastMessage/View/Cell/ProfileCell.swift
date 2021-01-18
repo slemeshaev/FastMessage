d25a080f73b2f8130fa63e895b740685a9b45fe2
slemeshaev <lemeshaevs@gmail.com>
2021-01-18 09:59:53 +0300

    Profile cell

--- FastMessage/FastMessage/View/Cell/ProfileCell.swift
+++ FastMessage/FastMessage/View/Cell/ProfileCell.swift
@@ -5,4 +5,17 @@
 //  Created by Станислав Лемешаев on 18.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class ProfileCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
