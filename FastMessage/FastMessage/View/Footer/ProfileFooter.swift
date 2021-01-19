d8ca32c2022f75ea6747b14d09a634597a5f1402
slemeshaev <lemeshaevs@gmail.com>
2021-01-19 10:45:04 +0300

    Setup logoutbutton

--- FastMessage/FastMessage/View/Footer/ProfileFooter.swift
+++ FastMessage/FastMessage/View/Footer/ProfileFooter.swift
@@ -5,4 +5,37 @@
 //  Created by Станислав Лемешаев on 19.01.2021.
 //
 
-import Foundation
+import UIKit
+
+class ProfileFooter: UIView {
+    
+    // MARK: - Properties
+    
+    private lazy var logoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Выход", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(logoutButton)
+        logoutButton.anchor(left: leftAnchor, right: rightAnchor,
+                            paddingLeft: 32, paddingRight: 32)
+        logoutButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        logoutButton.centerY(inView: self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
