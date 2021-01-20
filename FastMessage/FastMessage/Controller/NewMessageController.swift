0146936c65fa1af1dc304eb6c7fc9b11dc494da1
slemeshaev <lemeshaevs@gmail.com>
2021-01-20 20:55:01 +0300

    Реализация поиска польователей

--- FastMessage/FastMessage/Controller/NewMessageController.swift
+++ FastMessage/FastMessage/Controller/NewMessageController.swift
@@ -12,13 +12,20 @@ class NewMessageController: UITableViewController {
     // MARK: - Properties
     private static let reuseId = "UserCell"
     private var users = [User]()
+    private var filteredUsers = [User]()
     weak var delegate: NewMessageControllerDelegate?
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var inSearchMode: Bool {
+        return searchController.isActive && !searchController.searchBar.text!.isEmpty
+    }
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        configureSearchController()
         fetchUsers()
     }
     
@@ -37,6 +44,7 @@ class NewMessageController: UITableViewController {
     }
     
     // MARK: - Helpers
+    
     func configureUI() {
         configureNavigationBar(withTitle: "Новое сообщение", prefersLargeTitles: false)
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(handleDismissal))
@@ -45,24 +53,54 @@ class NewMessageController: UITableViewController {
         tableView.register(UserCell.self, forCellReuseIdentifier: NewMessageController.reuseId)
         tableView.rowHeight = 80
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.showsCancelButton = false
+        navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.hidesNavigationBarDuringPresentation = false
+        searchController.searchBar.placeholder = "Поиск польователей"
+        definesPresentationContext = false
+        
+        if let textField = searchController.searchBar.value(forKey: "searchField") as? UITextField {
+            textField.textColor = .systemPurple
+            textField.backgroundColor = .white
+        }
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
 extension NewMessageController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return inSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: NewMessageController.reuseId, for: indexPath) as! UserCell
-        cell.user = users[indexPath.row]
+        cell.user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
         return cell
     }
 }
 
 extension NewMessageController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.controller(self, wantsToStartChatWith: users[indexPath.row])
+        let user = inSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        delegate?.controller(self, wantsToStartChatWith: user)
     }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension NewMessageController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        filteredUsers = users.filter({ user -> Bool in
+            return user.userName.contains(searchText) || user.fullName.contains(searchText)
+        })
+        self.tableView.reloadData()
+    }
+    
 }
